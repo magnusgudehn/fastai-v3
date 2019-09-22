d474a577e9dfd85f51b0a85cb089e3a54c56e199
magnusgudehn <55056627+magnusgudehn@users.noreply.github.com>
2019-09-22 13:28:10 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/open?id=1HhMm8tiEyiHWnB_jCuXL-NXWgRC-j0wm'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1HhMm8tiEyiHWnB_jCuXL-NXWgRC-j0wm
 export_file_name = 'export.pkl'
 
 classes = ['Titleist', 'Top Flite', 'Callaway', 'Mozze']
